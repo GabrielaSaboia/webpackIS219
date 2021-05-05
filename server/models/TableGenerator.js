35f0fe066dacec29df02f886acd6b45d6c68c9db
gabrielasaboia <gs525@njit.edu>
2021-05-05 08:10:31 -0400

    fixed code logic in the table generator functions to output the correct data.

--- server/models/TableGenerator.js
+++ server/models/TableGenerator.js
@@ -2,24 +2,24 @@ class TableGenerator {
   static generateTableHead(table, data) {
     const thead = table.createTHead();
     const row = thead.insertRow();
-    const keys = Object.keys(data);
-    keys.forEach((key) => {
+    const values = Object.values(data);
+    console.log(values);
+    values.forEach((value) => {
       const th = document.createElement('th');
-      const text = document.createTextNode(key);
+      const text = document.createTextNode(value);
       th.appendChild(text);
       row.appendChild(th);
     });
   }
 
   static generateTable(table, data) {
-    const records = Object.entries(data);
-    records.forEach((record) => {
+    data.forEach((record) => {
       const row = table.insertRow();
       console.log(record);
-      const key = Object.keys(record);
-      key.forEach((k) => {
+      const values = Object.values(record);
+      values.forEach((value) => {
         const cell = row.insertCell();
-        const text = document.createTextNode(record[k]);
+        const text = document.createTextNode(value);
         cell.appendChild(text);
       });
     });
