63e932aee7bd3d6db325a7032a32091f47cc6898
gabrielasaboia <gs525@njit.edu>
2021-05-12 19:58:25 -0400

    Updated database added user table

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,24 @@
 CREATE DATABASE citiesData;
 use citiesData;
 
+CREATE TABLE IF NOT EXISTS tblUsersImport (
+    `id` int AUTO_INCREMENT,
+    `fName` VARCHAR(21) CHARACTER SET utf8,
+    `lName` VARCHAR(21) CHARACTER SET utf8,
+    `username` VARCHAR(30) CHARACTER SET utf8,
+    `password` VARCHAR(50) CHARACTER SET utf8,
+    `email` VARCHAR(100) CHARACTER SET utf8,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO tblUsersImport (fName,lName,username,password,email) VALUES
+('Gabriela', 'Saboia', 'GSaboia', '123456', 'g@gmail.com'),
+('Eileen', 'Wilson', 'WilsonE', '874291', 'EWilson@gmail.com'),
+('John', 'Stamos', 'Stamos_still', 'inittowinit', 'inittowinit@gmail.com'),
+('Zack', 'Efron', 'TheFron', 'jbdqiuw', 'thefron@gmail.com'),
+('Arabella', 'Martinez', 'Arabell', '00000000', 'bella@gmail.com'),
+('Elliot', 'Parker', 'EParker', 'Parkedout', 'pweezy@gmail.com');
+
 CREATE TABLE IF NOT EXISTS tblCitiesImport (
     `id` int AUTO_INCREMENT,
     `fldName` VARCHAR(21) CHARACTER SET utf8,
--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,14 +1,14 @@
 'user strict';
 
-const mysql = require('mysql');
+const mysql = require('mysql');//module import
 
 // local mysql db connection
-const dbConn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
+const dbConn = mysql.createConnection({ //connection function from module
+  host: 'localhost', //definition of local host
+  user: 'root', //definition of user
+  password: 'root', //password
   port: '32000',
-  database: 'citiesData',
+  database: 'citiesData', //database definition
 });
 dbConn.connect((err) => {
   if (err) throw err;
